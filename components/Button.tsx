cd117f4cc8dcc3a9a4efd98a59a7957270904bb0
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-11 23:13:10 +0800

    style(Button): adjust padding for improved layout consistency

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = () => {
   return (
-    <div className="w-full flex justify-end pr-2">
+    <div className="w-full flex justify-end pr-6">
       <button className="btn btn-success">Submit</button>
     </div>
   );
