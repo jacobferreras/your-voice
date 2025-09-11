26347795037d756020d69c08ca1a6805f3433a18
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-11 15:17:27 +0800

    update Home component to render Container instead of empty fragment

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Container from "@/components/Container";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Container />
+    </>
+  );
 }
