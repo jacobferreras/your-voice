6b6e1574aa77cbc58c6aff66743f8e8b6b57576b
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-11 15:17:33 +0800

    add Button component with submit functionality

---
+++ components/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div className="w-full flex justify-end pr-2">
+      <button className="btn btn-success">Submit</button>
+    </div>
+  );
+};
+
+export default Button;
