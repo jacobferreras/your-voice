4a101d3acfaaefcf7ff35a0da983d269f3a28750
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-12 20:01:15 +0800

    refactor(useFeedbackItems): replace local state management with zustand store

--- hooks/useFeedbackItems.tsx
+++ hooks/useFeedbackItems.tsx
@@ -1,10 +1,23 @@
 "use client";
 import fetchFeedback from "@/service/fetchFeedback";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import useFeedbackStore from "@/store/useFeedbackStore";
 
 const useFeedbackItems = () => {
-  const [feedbackItems, setFeedbackItems] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const [text, setText] = useState("");
+  // let remainingChars = MAX_CHAR_LIMIT - text.length;
+  // const [feedbackItems, setFeedbackItems] = useState([]);
+  // const [loading, setLoading] = useState(true);
+
+  // const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  //   e.preventDefault();
+  //   setText(e.target.value);
+  // };
+
+  const feedbackItems = useFeedbackStore((state) => state.feedbackItems);
+  const loading = useFeedbackStore((state) => state.loading);
+  const setFeedbackItems = useFeedbackStore((state) => state.setFeedbackItems);
+  const setLoading = useFeedbackStore((state) => state.setLoading);
 
   useEffect(() => {
     const getFeedbackItems = async () => {
