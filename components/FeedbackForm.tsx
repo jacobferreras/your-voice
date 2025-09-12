0cda20288c5cdcc7f237f1f174d5046844652801
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-12 20:04:19 +0800

    refactor(FeedbackForm): remove direct state management from FeedbackForm component

--- components/FeedbackForm.tsx
+++ components/FeedbackForm.tsx
@@ -2,13 +2,8 @@ import React from "react";
 import TextArea from "./TextArea";
 import WordsLimit from "./WordsLimit";
 import Button from "./Button";
-import useFeedbackStore from "@/store/useFeedbackStore";
+
 const FeedbackForm = () => {
-  const text = useFeedbackStore((state) => state.text);
-  const handleChange = useFeedbackStore((state) => state.handleChange);
-  const remainingChars = useFeedbackStore((state) =>
-    state.remainingChars(280, state.text)
-  );
   return (
     <>
       <div className="card bg-[#212121] text-neutral-content w-auto">
@@ -19,8 +14,8 @@ const FeedbackForm = () => {
           <p className="font-semibold text-xl">
             Enter your feedback here, remember to <b>#hashtag</b> the company..
           </p>
-          <TextArea text={text} handleChange={handleChange} />
-          <WordsLimit remainingChars={remainingChars} />
+          <TextArea />
+          <WordsLimit />
           <Button />
         </div>
       </div>
