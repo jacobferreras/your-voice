944fada48a6fcbb2cf671afc81a661d03023f807
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-12 20:05:22 +0800

    refactor(useFeedbackItems): remove unused local state and comments for cleaner code

--- hooks/useFeedbackItems.tsx
+++ hooks/useFeedbackItems.tsx
@@ -4,16 +4,6 @@ import { useEffect } from "react";
 import useFeedbackStore from "@/store/useFeedbackStore";
 
 const useFeedbackItems = () => {
-  // const [text, setText] = useState("");
-  // let remainingChars = MAX_CHAR_LIMIT - text.length;
-  // const [feedbackItems, setFeedbackItems] = useState([]);
-  // const [loading, setLoading] = useState(true);
-
-  // const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-  //   e.preventDefault();
-  //   setText(e.target.value);
-  // };
-
   const feedbackItems = useFeedbackStore((state) => state.feedbackItems);
   const loading = useFeedbackStore((state) => state.loading);
   const setFeedbackItems = useFeedbackStore((state) => state.setFeedbackItems);
