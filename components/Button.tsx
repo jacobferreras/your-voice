612fc12ab367d5593df843494f090874f9cc6bc2
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-12 22:47:22 +0800

    feat(Button): add feedback submission functionality to button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import usePostFeedback from "@/hooks/usePostFeedback";
 
 const Button = () => {
+  const { handlePostFeedback } = usePostFeedback();
+
   return (
     <div className="w-full flex justify-end pr-12">
-      <button className="btn btn-success">Submit</button>
+      <button className="btn btn-success" onClick={handlePostFeedback}>
+        Submit
+      </button>
     </div>
   );
 };
