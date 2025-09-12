2d83c77e7e7728f6749724b062156245a4b782f6
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-12 20:04:23 +0800

    refactor(TextArea): remove props and use zustand store for state management

--- components/TextArea.tsx
+++ components/TextArea.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { MAX_CHAR_LIMIT } from "@/lib/constant";
+import useFeedbackStore from "@/store/useFeedbackStore";
 
-interface TextAreaProps {
-  text: string;
-  handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
-}
+const TextArea = () => {
+  const text = useFeedbackStore((state) => state.text);
+  const handleChange = useFeedbackStore((state) => state.handleChange);
 
-const TextArea = ({ text, handleChange }: TextAreaProps) => {
   return (
     <textarea
       value={text}
