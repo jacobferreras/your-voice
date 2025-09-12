907462ad6bffc7e4ba1b2df6b90c1380b54a1adc
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-12 22:47:26 +0800

    feat(usePostFeedback): implement feedback posting logic and state management

--- hooks/usePostFeedback.tsx
+++ hooks/usePostFeedback.tsx
@@ -1,6 +1,27 @@
 import { postFeedback } from "@/service/postFeedback";
 import useFeedbackStore from "@/store/useFeedbackStore";
 
-const usePostFeedback = () => {};
+const usePostFeedback = () => {
+  const addFeedbackItem = useFeedbackStore((state) => state.addFeedbackItem);
+  const text = useFeedbackStore((state) => state.text);
+
+  const handlePostFeedback = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    const companyName = text.split(" ").find((word) => word.startsWith("#"));
+    const newItem = {
+      id: Date.now(),
+      upvoteCount: 0,
+      company: companyName ? companyName.replace("#", "") : "Example Corp",
+      daysAgo: 0,
+      text,
+      badgeLetter: companyName ? companyName.charAt(1).toUpperCase() : "E",
+    };
+    addFeedbackItem(newItem);
+    await postFeedback(newItem);
+  };
+
+  return { handlePostFeedback };
+};
 
 export default usePostFeedback;
