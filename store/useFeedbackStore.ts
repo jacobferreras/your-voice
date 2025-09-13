bc51c29cb9c7e0711657108f7d1cdd9755c19c32
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-13 20:05:05 +0800

    refactor(useFeedbackStore): enhance feedback handling by adding fetchFeedback integration and streamlining state management

--- store/useFeedbackStore.ts
+++ store/useFeedbackStore.ts
@@ -1,42 +1,87 @@
 import { create } from "zustand";
 import { FeedbackItem } from "../types/feedbackItems";
+import { postFeedback } from "../service/postFeedback";
+import fetchFeedback from "@/service/fetchFeedback";
 
 interface FeedbackStore {
   text: string;
-  setText: (text: string) => void;
   borderColor: string;
-  setBorderColor: (color: string) => void;
-  handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  remainingChars: (MAX_CHAR_LIMIT: number, text: string) => number;
   feedbackItems: FeedbackItem[];
   loading: boolean;
-  setFeedbackItems: (items: FeedbackItem[]) => void;
-  setLoading: (loading: boolean) => void;
-  addFeedbackItem: (item: FeedbackItem) => void;
   selectedCompany?: string;
+  handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  remainingChars: (MAX_CHAR_LIMIT: number, text: string) => number;
   selectCompany: (company: string) => void;
   clearCompany: () => void;
+  getAction: () => void;
+  addAction: (text: string) => void;
 }
 
 const useFeedbackStore = create<FeedbackStore>((set) => ({
   text: "",
-  setText: (text: string) => set({ text }),
+
   borderColor: "border-gray-600",
-  setBorderColor: (color: string) => set({ borderColor: color }),
+
+  loading: true,
+
+  feedbackItems: [] as FeedbackItem[],
+
   handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     e.preventDefault();
     set({ text: e.target.value });
   },
+
   remainingChars: (MAX_CHAR_LIMIT: number, text: string) =>
     MAX_CHAR_LIMIT - text.length,
-  feedbackItems: [] as FeedbackItem[],
-  loading: true,
+
   setFeedbackItems: (items: FeedbackItem[]) => set({ feedbackItems: items }),
-  setLoading: (loading) => set({ loading }),
-  addFeedbackItem: (item: FeedbackItem) =>
-    set((state) => ({ feedbackItems: [...state.feedbackItems, item] })),
+
   selectCompany: (company: string) => set({ selectedCompany: company }),
+
   clearCompany: () => set({ selectedCompany: "" }),
+
+  getAction: async () => {
+    try {
+      set({ loading: true });
+      const feedbackItems = await fetchFeedback();
+      set({ feedbackItems });
+    } catch (error) {
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  addAction: async (text: string) => {
+    const companyRawName = text
+      .split(" ")
+      .find((word) => word.startsWith("#"))
+      ?.substring(1);
+
+    const companyName = companyRawName
+      ? companyRawName.charAt(0).toUpperCase() +
+        companyRawName.slice(1).toLowerCase()
+      : "";
+
+    if (text.trim() === "" || !companyName) {
+      set({ borderColor: "border-red-600", text: "" });
+
+      return;
+    }
+
+    set({ borderColor: "border-green-600", text: "" });
+
+    const newItem = {
+      id: Date.now(),
+      upvoteCount: 0,
+      company: companyName ? companyName.replace("#", "") : "Example Corp",
+      daysAgo: 0,
+      text,
+      badgeLetter: companyName ? companyName.charAt(1).toUpperCase() : "E",
+    };
+
+    set((state) => ({ feedbackItems: [...state.feedbackItems, newItem] }));
+    await postFeedback(newItem);
+  },
 }));
 
 export default useFeedbackStore;
