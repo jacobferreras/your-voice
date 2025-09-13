1e3a1f8890722b91bceab9e8c8e767b69274d482
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-13 13:52:46 +0800

    refactor(Sidebar): add props for company selection and remove internal hook usage

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,11 @@
 import useFeedbackItems from "../hooks/useFeedbackItems";
 
-const Sidebar = () => {
-  const { companyList } = useFeedbackItems();
+interface SidebarProps {
+  handleSelectCompany: (company: string) => void;
+  companyList: string[];
+}
+
+const Sidebar = ({ handleSelectCompany, companyList }: SidebarProps) => {
   return (
     <>
       <div className="card card-border bg-[#212121] w-50 h-216">
@@ -9,7 +13,10 @@ const Sidebar = () => {
           <ul className="menu gap-4">
             {companyList.map((company) => (
               <li key={company}>
-                <button className="btn btn-soft rounded-xl w-auto">
+                <button
+                  onClick={() => handleSelectCompany(company)}
+                  className="btn btn-soft rounded-xl w-auto"
+                >
                   #{company}
                 </button>
               </li>
